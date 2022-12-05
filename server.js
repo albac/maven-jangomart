91d99c463c2f8a3e5170faf9934ab65deb3cb5b6
Alfredo Baldoceda <alfredo.baldoceda@nomissolutions.com>
2022-12-05 02:53:06 -0500

    new vars

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const pg = require("pg");
 const express = require("express");
 const app = express();
-const port = 8080;
+const port = 5000;
 
 console.log("The value of User is:", process.env.POSTGRES_USER);
 console.log("The value of DB is:", process.env.POSTGRES_DBNAME);
+
+const client = new Client({
+  password: process.env.POSTGRES_PASSWORD,
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_DBNAME,
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
